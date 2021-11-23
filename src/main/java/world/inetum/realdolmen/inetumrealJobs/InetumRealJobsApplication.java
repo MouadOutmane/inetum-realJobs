b63a050a8964628dc2733123dd39e67b1e971beb
MouadOutmane <mouad.outmane@realdolmen.com>
2021-11-23 11:12:19 +0100

    Spring initializr

---
+++ src/main/java/world/inetum/realdolmen/inetumrealJobs/InetumRealJobsApplication.java
@@ -0,0 +1,13 @@
+package world.inetum.realdolmen.inetumrealJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InetumRealJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InetumRealJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/world/inetum/realdolmen/inetumrealJobs/InetumRealJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package world.inetum.realdolmen.inetumrealJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InetumRealJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
