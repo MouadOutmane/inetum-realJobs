ee5c0e4ea28eaf19c61e7c0f37a650fb345948d9
Tom De bruyn <tom.de.bruyn99@gmail.com>
2021-12-14 13:40:59 +0100

    Removed unused import and reformatted pom and readme

--- src/main/java/world/inetum/realdolmen/inetumrealJobs/services/UserDetailsImpl.java
+++ src/main/java/world/inetum/realdolmen/inetumrealJobs/services/UserDetailsImpl.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
 
