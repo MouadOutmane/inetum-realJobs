9eb9074d4c84a9a03b0cb2f29afcd5e64cf8557f
seppeLenaerts <43441205+seppeLenaerts@users.noreply.github.com>
2021-12-14 15:55:39 +0100

    Changed modelmapper to mapstruct and added implementation

--- src/main/java/world/inetum/realdolmen/inetumrealJobs/mappers/VacancyFilterResultMapper.java
+++ src/main/java/world/inetum/realdolmen/inetumrealJobs/mappers/VacancyFilterResultMapper.java
@@ -1,5 +1,12 @@
 package world.inetum.realdolmen.inetumrealJobs.mappers;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import world.inetum.realdolmen.inetumrealJobs.dtos.VacancyFilterResultDto;
+import world.inetum.realdolmen.inetumrealJobs.entities.Vacancy;
 
-public class VacancyFilterResultMapper {
+@Mapper(componentModel = "cdi", uses = VacancyFilterResultMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface VacancyFilterResultMapper {
+
+    VacancyFilterResultDto toDto(Vacancy vacancy);
 
 }
