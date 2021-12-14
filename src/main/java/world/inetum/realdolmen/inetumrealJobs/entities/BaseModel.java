ed6c5a6a9493781271d3eba74e5b29cad3799fc9
Tom De bruyn <tom.de.bruyn99@gmail.com>
2021-12-14 14:01:44 +0100

    Fix audit

--- src/main/java/world/inetum/realdolmen/inetumrealJobs/entities/BaseModel.java
+++ src/main/java/world/inetum/realdolmen/inetumrealJobs/entities/BaseModel.java
@@ -4,11 +4,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseModel {
 
     @Id
@@ -16,23 +18,23 @@ public abstract class BaseModel {
     private Long id;
 
     @Version
-    @Column(name = "VERSION", nullable = false)
+    @Column(name = "version", nullable = false)
     private int version;
 
     @CreatedBy
-    @Column(name = "CREATED_BY", updatable = false)
+    @Column(name = "created_by", updatable = false)
     private String createdBy;
 
     @CreatedDate
-    @Column(name = "CREATED_ON", updatable = false)
+    @Column(name = "created_on", updatable = false)
     private LocalDateTime createdOn;
 
     @LastModifiedBy
-    @Column(name = "CHANGED_BY")
+    @Column(name = "changed_by")
     private String changedBy;
 
     @LastModifiedDate
-    @Column(name = "CHANGED_ON")
+    @Column(name = "changed_on")
     private LocalDateTime changedOn;
 
     public Long getId() {
