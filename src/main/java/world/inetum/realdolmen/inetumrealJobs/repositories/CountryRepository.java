efbdef7597ca3a8e7e2fd527b6cdd4e727dcaedd
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-21 10:46:25 +0100

    Include a test database, created through docker compose.

--- src/main/java/world/inetum/realdolmen/inetumrealJobs/repositories/CountryRepository.java
+++ src/main/java/world/inetum/realdolmen/inetumrealJobs/repositories/CountryRepository.java
@@ -3,10 +3,13 @@ package world.inetum.realdolmen.inetumrealJobs.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import world.inetum.realdolmen.inetumrealJobs.entities.Country;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
     Optional<Country> findByName(String name);
+
+    List<Country> findAllByOrderByName();
 
 }
