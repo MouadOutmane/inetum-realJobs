5bf8549cfe352f5041dea6aa37bf10c89c54458b
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-22 11:13:03 +0100

    Improve tests further

--- inetum-realjobs-backend/src/test/java/world/inetum/realdolmen/realjobs/controllers/VacancyControllerIT.java
+++ inetum-realjobs-backend/src/test/java/world/inetum/realdolmen/realjobs/controllers/VacancyControllerIT.java
@@ -24,8 +24,18 @@ public class VacancyControllerIT extends BaseIntegrationTest {
 
     @Test
     public void findAllVacanciesWithFilter_GETVacancyNoMatch_NoContent() throws Exception {
-        mockMvc.perform(get("/api/vacancies/?functionTitle=&contractType=&industry=&country=7348&requiredYearsOfExperience=0"))
-                .andExpect(status().isNoContent());
+        mockMvc.perform(
+                        get("/api/vacancies")
+                                .queryParam("functionTitle", "")
+                                .queryParam("contractType", "")
+                                .queryParam("industry", "")
+                                .queryParam("country", "726")
+                                .queryParam("requiredYearsOfExperience", "0")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isEmpty());
     }
 
     @Test
@@ -48,16 +58,42 @@ public class VacancyControllerIT extends BaseIntegrationTest {
         persistVacancy("function8", ContractType.FULL_TIME, createAddress(country3), company3, 0);
 
         mockMvc.perform(
-                        get("/api/vacancies/?functionTitle=&contractType=&industry=&country=&requiredYearsOfExperience=3")
+                        get("/api/vacancies")
+                                .queryParam("functionTitle", "")
+                                .queryParam("contractType", "")
+                                .queryParam("industry", "")
+                                .queryParam("country", String.valueOf(country1.getId()))
+                                .queryParam("requiredYearsOfExperience", "3")
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(3));
     }
 
     @Test
-    public void findAllVacancies_GET_Success() throws Exception{
+    public void findAllVacancies_GET_Success() throws Exception {
+        Country country1 = persistCountry("Belgium");
+        Country country2 = persistCountry("France");
+        Country country3 = persistCountry("Germany");
+
+        Company company1 = persistCompany("name1", "industry1", country1);
+        Company company2 = persistCompany("name2", "industry2", country2);
+        Company company3 = persistCompany("name3", "industry3", country3);
+
+        persistVacancy("function1", ContractType.PART_TIME, createAddress(country1), company1, 1);
+        persistVacancy("function2", ContractType.PART_TIME, createAddress(country1), company1, 3);
+        persistVacancy("function3", ContractType.FLEXI, createAddress(country2), company2, 0);
+        persistVacancy("function4", ContractType.FULL_TIME, createAddress(country1), company1, 0);
+        persistVacancy("function5", ContractType.FULL_TIME, createAddress(country2), company2, 4);
+        persistVacancy("function6", ContractType.FULL_TIME, createAddress(country1), company1, 8);
+        persistVacancy("function7", ContractType.FULL_TIME, createAddress(country2), company2, 7);
+        persistVacancy("function8", ContractType.FULL_TIME, createAddress(country3), company3, 0);
+
         mockMvc.perform(get("/api/vacancies/all"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(8));
     }
 
     @Test
@@ -101,6 +137,7 @@ public class VacancyControllerIT extends BaseIntegrationTest {
                                 .content(asJsonString(vacancy))
                 )
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty())
                 .andExpect(jsonPath("$.functionTitle").value("Vendor"));
     }
 
