e5a1116667e3fba506c7ffa2be8314517483a1a2
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-22 16:14:16 +0100

    Move PrimeNg imports to their own file

--- inetum-realjobs-frontend/src/app/app.module.ts
+++ inetum-realjobs-frontend/src/app/app.module.ts
@@ -10,20 +10,11 @@ import {UserDetailsComponent} from "./components/user-details/user-details.compo
 import {UniversalAppInterceptor} from "src/app/interceptor/universalAppInterceptor";
 import {AuthGuardService} from "./guard/authGuard.service";
 import {RegisterComponent} from "./components/register/register.component";
-import {PasswordModule} from "primeng/password";
-import {PanelModule} from "primeng/panel";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {DropdownModule} from "primeng/dropdown";
-import {CalendarModule} from "primeng/calendar";
-import {FileUploadModule} from "primeng/fileupload";
-import {RadioButtonModule} from "primeng/radiobutton";
-import {MessagesModule} from "primeng/messages";
-import {MessageModule} from "primeng/message";
-import {ToastModule} from "primeng/toast";
-import {InputMaskModule} from "primeng/inputmask";
 import {NotFoundComponent} from "./components/not-found/not-found.component";
 import {VacancyDetailsComponent} from "./components/vacancy-details/vacancy-details.component";
 import {ErrorHandlerComponent} from "./components/error-handler/error-handler.component";
+import {PrimengImportsModule} from "./primeng-imports";
 
 @NgModule({
   declarations: [
@@ -41,18 +32,9 @@ import {ErrorHandlerComponent} from "./components/error-handler/error-handler.co
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    PasswordModule,
-    PanelModule,
-    DropdownModule,
-    CalendarModule,
-    RadioButtonModule,
-    MessagesModule,
-    MessageModule,
     BrowserAnimationsModule,
-    FileUploadModule,
-    ToastModule,
-    InputMaskModule,
     routing,
+    PrimengImportsModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: UniversalAppInterceptor, multi: true},
--- inetum-realjobs-frontend/src/app/primeng-imports.ts
+++ inetum-realjobs-frontend/src/app/primeng-imports.ts
@@ -2,14 +2,37 @@ import {NgModule} from "@angular/core";
 import {ButtonModule} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
 import {PanelModule} from "primeng/panel";
+import {CardModule} from "primeng/card";
+import {DropdownModule} from "primeng/dropdown";
+import {CalendarModule} from "primeng/calendar";
+import {FileUploadModule} from "primeng/fileupload";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {MessagesModule} from "primeng/messages";
+import {MessageModule} from "primeng/message";
+import {ToastModule} from "primeng/toast";
+import {InputMaskModule} from "primeng/inputmask";
+import {PasswordModule} from "primeng/password";
+
+const modules = [
+  ButtonModule,
+  InputTextModule,
+  PanelModule,
+  CardModule,
+  DropdownModule,
+  CalendarModule,
+  RadioButtonModule,
+  MessagesModule,
+  MessageModule,
+  FileUploadModule,
+  ToastModule,
+  InputMaskModule,
+  PasswordModule,
+  PanelModule,
+];
 
 @NgModule({
-  imports: [
-    ButtonModule,
-    InputTextModule,
-    PanelModule,
-  ],
-  declarations: [],
+  imports: modules,
+  exports: modules,
 })
 export class PrimengImportsModule {
 }
