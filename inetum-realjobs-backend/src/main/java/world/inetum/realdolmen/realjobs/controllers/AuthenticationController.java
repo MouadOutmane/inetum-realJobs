0cdd74116005395dee194c3e92d4ba5ba40da4c7
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-22 10:45:07 +0100

    Improve authentication tests

--- inetum-realjobs-backend/src/main/java/world/inetum/realdolmen/realjobs/controllers/AuthenticationController.java
+++ inetum-realjobs-backend/src/main/java/world/inetum/realdolmen/realjobs/controllers/AuthenticationController.java
@@ -7,18 +7,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import world.inetum.realdolmen.realjobs.payload.security.LoginRequest;
-import world.inetum.realdolmen.realjobs.payload.security.SignupRequest;
 import world.inetum.realdolmen.realjobs.payload.security.JwtResponse;
+import world.inetum.realdolmen.realjobs.payload.security.LoginRequest;
 import world.inetum.realdolmen.realjobs.payload.security.MessageResponse;
+import world.inetum.realdolmen.realjobs.payload.security.SignupRequest;
+import world.inetum.realdolmen.realjobs.security.UserDetailsImpl;
 import world.inetum.realdolmen.realjobs.security.jwt.JwtUtils;
 import world.inetum.realdolmen.realjobs.services.AccountService;
-import world.inetum.realdolmen.realjobs.security.UserDetailsImpl;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
--- inetum-realjobs-backend/src/test/java/world/inetum/realdolmen/realjobs/controllers/AuthenticationControllerIT.java
+++ inetum-realjobs-backend/src/test/java/world/inetum/realdolmen/realjobs/controllers/AuthenticationControllerIT.java
@@ -16,19 +16,24 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import world.inetum.realdolmen.realjobs.BaseIntegrationTest;
 import world.inetum.realdolmen.realjobs.InetumRealJobsApplication;
-import world.inetum.realdolmen.realjobs.payload.dtos.AddressReadDto;
+import world.inetum.realdolmen.realjobs.entities.Account;
+import world.inetum.realdolmen.realjobs.entities.Country;
+import world.inetum.realdolmen.realjobs.entities.JobSeeker;
+import world.inetum.realdolmen.realjobs.entities.Recruiter;
 import world.inetum.realdolmen.realjobs.entities.enums.Gender;
 import world.inetum.realdolmen.realjobs.entities.enums.Role;
 import world.inetum.realdolmen.realjobs.exceptions.EndpointException;
 import world.inetum.realdolmen.realjobs.exceptions.messages.SignUpExceptionMessage;
+import world.inetum.realdolmen.realjobs.payload.dtos.AddressReadDto;
 import world.inetum.realdolmen.realjobs.payload.security.LoginRequest;
 import world.inetum.realdolmen.realjobs.payload.security.SignupRequest;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,7 +60,7 @@ class AuthenticationControllerIT extends BaseIntegrationTest {
 
     @Test
     void loginWithExistingCredentialsReturning200() throws Exception {
-        persistJobSeeker("test@inetum-realdolmen.world", "password");
+        Account account = persistJobSeeker("test@inetum-realdolmen.world", "password");
 
         LoginRequest request = new LoginRequest("test@inetum-realdolmen.world", "password");
 
@@ -64,7 +69,14 @@ class AuthenticationControllerIT extends BaseIntegrationTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(mapper.writeValueAsString(request))
                 )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").isNotEmpty())
+                .andExpect(jsonPath("$.tokenType").value("Bearer"))
+                .andExpect(jsonPath("$.id").value(account.getId()))
+                .andExpect(jsonPath("$.email").value("test@inetum-realdolmen.world"))
+                .andExpect(jsonPath("$.roles").isArray())
+                .andExpect(jsonPath("$.roles.length()").value(1))
+                .andExpect(jsonPath("$.roles[0]").value("ROLE_JOBSEEKER"));
     }
 
     @Test
@@ -80,12 +92,13 @@ class AuthenticationControllerIT extends BaseIntegrationTest {
 
     @Test
     void registerWithNonExistingCredentialsReturning200() throws Exception {
+        Country country = persistCountry("some country");
 
         AddressReadDto addressDto = new AddressReadDto();
         addressDto.setStreetName("street name");
         addressDto.setHouseNumber("15");
         addressDto.setCity("city");
-        addressDto.setCountry(20L);
+        addressDto.setCountry(country.getId());
         addressDto.setPostalCode("1000");
 
         SignupRequest request = new SignupRequest();
@@ -103,12 +116,83 @@ class AuthenticationControllerIT extends BaseIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
+
+        Optional<Account> optionalAccount = accountRepository.findByEmail("user2@user.user");
+        assertTrue(optionalAccount.isPresent(), "Account was not found in the database");
+
+        Account account = optionalAccount.get();
+        assertAll(() -> {
+            assertNotEquals("password", account.getPassword(), "Password should not have saved in plain text.");
+            assertInstanceOf(JobSeeker.class, account, "Didn't apply correct role");
+        });
+    }
+
+    @Test
+    void registerWithNonExistingRecruiterCredentialsReturning200() throws Exception {
+        Country country = persistCountry("some country");
+
+        AddressReadDto addressDto = new AddressReadDto();
+        addressDto.setStreetName("street name");
+        addressDto.setHouseNumber("15");
+        addressDto.setCity("city");
+        addressDto.setCountry(country.getId());
+        addressDto.setPostalCode("1000");
+
+        SignupRequest request = new SignupRequest();
+        request.setEmail("user2@user.user");
+        request.setPassword("password");
+        request.setRole(Role.RECRUITER);
+        request.setGender(Gender.MALE);
+        request.setFirstName("User");
+        request.setLastName("User");
+        request.setAddress(addressDto);
+        request.setBirthDate(LocalDate.of(2021, 12, 10));
+        request.setMobilePhone("+32999999999");
+
+        mockMvc.perform(post("/api/authentication/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+
+        Optional<Account> optionalAccount = accountRepository.findByEmail("user2@user.user");
+        assertTrue(optionalAccount.isPresent(), "Account was not found in the database");
+
+        Account account = optionalAccount.get();
+        assertAll(() -> {
+            assertNotEquals("password", account.getPassword(), "Password should not have saved in plain text.");
+            assertInstanceOf(Recruiter.class, account, "Didn't apply correct role");
+        });
     }
 
     @Test
     void registerWithExistingCredentialsReturning401() {
         persistJobSeeker("user@user.user", "password");
+        Country country = persistCountry("some country");
 
+        AddressReadDto addressDto = new AddressReadDto();
+        addressDto.setStreetName("street name");
+        addressDto.setHouseNumber("15");
+        addressDto.setCity("city");
+        addressDto.setCountry(country.getId());
+        addressDto.setPostalCode("1000");
+
+        SignupRequest request = new SignupRequest();
+        request.setEmail("user@user.user");
+        request.setPassword("password");
+        request.setRole(Role.JOB_SEEKER);
+        request.setGender(Gender.MALE);
+        request.setFirstName("User");
+        request.setLastName("User");
+        request.setAddress(addressDto);
+        request.setBirthDate(LocalDate.of(2021, 12, 10));
+        request.setMobilePhone("+32999999999");
+
+        EndpointException exception = assertThrows(EndpointException.class, () -> authenticationController.signUp(request));
+        assertEquals(SignUpExceptionMessage.EMAIL_ALREADY_USED, exception.getExceptionMessage());
+    }
+
+    @Test
+    void registerWithFakeCountry() {
         AddressReadDto addressDto = new AddressReadDto();
         addressDto.setStreetName("street name");
         addressDto.setHouseNumber("15");
@@ -127,8 +211,8 @@ class AuthenticationControllerIT extends BaseIntegrationTest {
         request.setBirthDate(LocalDate.of(2021, 12, 10));
         request.setMobilePhone("+32999999999");
 
-        EndpointException exception = assertThrows(EndpointException.class, () -> authenticationController.signUp(request));
-        assertEquals(SignUpExceptionMessage.EMAIL_ALREADY_USED, exception.getExceptionMessage());
+        var exception = assertThrows(EndpointException.class, () -> authenticationController.signUp(request));
+        assertEquals(SignUpExceptionMessage.INCORRECT_DATA, exception.getExceptionMessage());
     }
 
 }
