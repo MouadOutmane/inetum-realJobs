a851b9161413e7232633ad1aa694e159c80dbf4f
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-28 14:32:24 +0100

    Don't expire the token immediately

--- inetum-realjobs-backend/src/main/java/world/inetum/realdolmen/realjobs/security/jwt/JwtUtils.java
+++ inetum-realjobs-backend/src/main/java/world/inetum/realdolmen/realjobs/security/jwt/JwtUtils.java
@@ -18,7 +18,7 @@ public class JwtUtils {
     @Value("password")
     private String jwtSecret;
 
-    @Value("2")
+    @Value("2000000")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
