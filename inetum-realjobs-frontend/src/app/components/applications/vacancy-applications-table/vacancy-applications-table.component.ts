b7d5e4cfd305a21180a3c835f0a3189e7e152c45
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-29 14:57:03 +0100

    Implement error handling

--- inetum-realjobs-frontend/src/app/components/applications/vacancy-applications-table/vacancy-applications-table.component.ts
+++ inetum-realjobs-frontend/src/app/components/applications/vacancy-applications-table/vacancy-applications-table.component.ts
@@ -2,18 +2,21 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Application} from "../../../models/application";
 import {ApplicationStatus} from "../../../models/application-status.enum";
 import {ApplicationService} from "../../../services/application.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: "app-vacancy-applications-table",
   templateUrl: "./vacancy-applications-table.component.html",
   styleUrls: ["./vacancy-applications-table.component.scss"],
+  providers: [MessageService],
 })
 export class VacancyApplicationsTableComponent {
 
   @Input() applications: Application[] = [];
   @Output() updateApplications = new EventEmitter<void>();
 
-  constructor(private applicationService: ApplicationService) {
+  constructor(private applicationService: ApplicationService,
+              private messageService: MessageService) {
   }
 
   get status(): typeof ApplicationStatus {
@@ -29,7 +32,22 @@ export class VacancyApplicationsTableComponent {
           this.updateApplications.emit();
         },
         error: (error) => {
-          // FIXME - Show error.
+          let message;
+          if (error?.error?.message) {
+            switch (error.error.message) {
+              case "ID_MISMATCH":
+              case "NOT_FOUND":
+                message = "Something went wrong. Please refresh and try again.";
+                break;
+              default:
+                message = "Something went wrong.";
+                break;
+            }
+          } else {
+            message = "Something went wrong.";
+          }
+
+          this.messageService.add({key: "tl", severity: "error", summary: "Error", detail: message});
           application.loading = false;
         },
       });
