fc56a6182812ca3532aae08795e27bb848110041
Johan Schoubben <Johan.Schoubben@inetum-realdolmen.world>
2022-01-14 13:47:20 +0100

    refactor navbar component code

--- inetum-realjobs-frontend/src/app/components/navbar/navbar.component.ts
+++ inetum-realjobs-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../services/authentication.service";
 import {MenuItem} from "primeng/api";
 import {RecruiterService} from "../../services/recruiter.service";
@@ -18,24 +18,29 @@ export class NavbarComponent implements OnInit {
   amountOfNotifications$: Observable<number>;
 
   constructor(private auth: AuthenticationService,
-              private recruiterService: RecruiterService) { }
+              private recruiterService: RecruiterService) {
+  }
 
   ngOnInit(): void {
     this.username = this.auth.getLoggedInUserEmail();
     this.avatarItems = [
-      {label: this.username, items: [{label: 'Account', icon: 'pi pi-user', routerLink: '../../users/' + this.username},
+      {
+        label: this.username,
+        items: [
+          {label: 'Account', icon: 'pi pi-user', routerLink: '../../users/' + this.username},
           {label: 'Log out', icon: 'pi pi-sign-out', command: () => {
-              this.auth.logout()
-            }, routerLink: '../../vacancy/search'
-          }]},
-
+              this.auth.logout()}, routerLink: '../../vacancy/search'}
+        ]},
     ];
     this.notificationItems = [
-      {label: 'Notifications', items: [{label: this.notifications[1]}]}
+      {label: 'Notifications',
+        items: [
+          {label: this.notifications[1]}
+        ]}
     ];
   }
 
-  getAmountOfUpdates(): Observable<number>{
+  getAmountOfUpdates(): Observable<number> {
     this.amountOfNotifications$ = this.recruiterService.getAmountOfNotifications();
     this.amountOfNotifications$.subscribe(x => this.amountOfNotifications = x);
     return this.amountOfNotifications$;
