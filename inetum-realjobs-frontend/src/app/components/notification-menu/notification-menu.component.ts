9cf5ff6b533f1fcb1f711ed7bdc82a8e67825af4
Johan Schoubben <Johan.Schoubben@inetum-realdolmen.world>
2022-01-18 10:06:50 +0100

    remove child routing for notifications

--- inetum-realjobs-frontend/src/app/components/notification-menu/notification-menu.component.ts
+++ inetum-realjobs-frontend/src/app/components/notification-menu/notification-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-notification-menu',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification-menu.component.scss']
 })
 export class NotificationMenuComponent implements OnInit {
+  @Input() notificationTemplateRef: any;
 
   constructor() { }
 
